b3863f4d90ead25bf2733e46ddccc51cb86744c8
Vladislav Kotsev <vladygk8@gmail.com>
2023-02-05 23:51:16 +0200

    Design improved

--- src/components/ReadyScreen.jsx
+++ src/components/ReadyScreen.jsx
@@ -1,23 +1,28 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../styles/ReadyScreen.module.css";
 import DonutComponent from "./DonutComponent";
-import donutImgs from "../assets/imports"
-export default function ReadyScreen(){
-const navigate = useNavigate();
+import donutImgs from "../assets/imports";
+export default function ReadyScreen() {
+  const navigate = useNavigate();
 
-function goToStart(){
+  function goToStart() {
     return navigate("/");
-}
+  }
 
-
-const location = useLocation();
-    return <div className={styles["ready-screen-container"]}>
-    <DonutComponent img={donutImgs[location.state.donut]} id="6"/>
-    <div className={styles.message}>
-        <p>You can now come</p>
-        <p>and pick up</p>
-        <p>your amazing Donut!</p>
+  const location = useLocation();
+  return (
+    <div className={styles["ready-screen-container"]}>
+      <DonutComponent img={donutImgs[location.state.donut]} id="6" />
+      <div className={styles.message}>
+        <div className={styles["paragraphs"]}>
+          <p>You can now come</p>
+          <p>and pick up</p>
+          <p>your amazing Donut!</p>
+        </div>
+      </div>
+      <button onClick={goToStart} className={styles.doneBtn}>
+        Done
+      </button>
     </div>
-    <button onClick={goToStart} className={styles.doneBtn}>Done</button>
-    </div>
+  );
 }
